a03f9ef2d7e830c15b65259984ccd78cdf1d922b
briz201205-glitch <briz201205@gmail.com>
2026-08-25 15:57:00 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.kotlin.compose) apply false
-  // Hapus atau comment baris ksp ini:
-  // alias(libs.plugins.google.devtools.ksp) apply false
+  alias(libs.plugins.google.devtools.ksp) apply false // <--- Biarkan aktif, jangan di-comment!
   alias(libs.plugins.roborazzi) apply false
   alias(libs.plugins.secrets) apply false
   alias(libs.plugins.google.services) apply false
